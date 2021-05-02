c65d9de5e2ef85d5dd69f95d0c1fddf01ebb3b81
hguruman <hguruman@gmail.com>
2021-05-02 10:43:17 +0400

    Setup index page for eduversity

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('partials.head')
+    </head>
+    <body class="layout-navbar-mini-fixed-bottom">
+        @include('partials.preloader')
+        <!-- Header Layout -->
+        <div class="mdk-header-layout js-mdk-header-layout">
+            @include('partials.navbar')
+
+            @yield('content')
+        </div>
+        @include('partials.bottom-navbar')
+        @include('partials.drawer')
+        <!-- App Settings FAB -->
+        @include('partials.app-setting')
+
+        @include('partials.footer')
+        
+        <!-- jQuery -->
+        <script src="{{ asset('assets/vendor/jquery.min.js') }}"></script>
+
+        <!-- Bootstrap -->
+        <script src="{{ asset('assets/vendor/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/bootstrap.min.js') }}"></script>
+
+        <!-- Perfect Scrollbar -->
+        <script src="{{ asset('assets/vendor/perfect-scrollbar.min.js') }}"></script>
+
+        <!-- DOM Factory -->
+        <script src="{{ asset('assets/vendor/dom-factory.js') }}"></script>
+
+        <!-- MDK -->
+        <script src="{{ asset('assets/vendor/material-design-kit.js') }}"></script>
+
+        <!-- Fix Footer -->
+        <script src="{{ asset('assets/vendor/fix-footer.js') }}"></script>
+
+        <!-- Chart.js -->
+        <script src="{{ asset('assets/vendor/Chart.min.js') }}"></script>
+
+        <!-- App JS -->
+        <script src="{{ asset('assets/js/app.js') }}"></script>
+
+        <!-- Highlight.js -->
+        <script src="{{ asset('assets/js/hljs.js') }}"></script>
+
+        <!-- App Settings (safe to remove) -->
+        <script src="{{ asset('assets/js/app-settings.js') }}"></script>
+    </body>
+</html>
---
+++ resources/views/partials/app-setting.blade.php
@@ -0,0 +1,10 @@
+<!-- App Settings FAB -->
+<div id="app-settings">
+    <app-settings theme-active="blue-light" :theme-location="{
+    'blue-light': 'http://tutorio-bootstrap.frontendmatter.com/library-development.html',
+    'blue-dark': 'http://tutorio-dark.frontendmatter.com/library-development.html',
+    'teal-light': 'http://tutorio-teal.frontendmatter.com/library-development.html',
+    'teal-dark': 'http://tutorio-teal-dark.frontendmatter.com/library-development.html'
+    }" sidebar-variant="bg-transparent border-0"></app-settings>
+</div>
+
